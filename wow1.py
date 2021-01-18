762278fabc2ee628c84ae6069258353fd46d142e
Pritish-Wakhare <pritish2127@gmail.com>
2021-01-18 11:09:24 +0530

    Add files via upload

---
+++ wow1.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import pyttsx3
+
+engine = pyttsx3.init()
+
+class Widget():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('TTS')
+        self.root.resizable(0, 0)
+        self.labell = tk.Label(self.root, text='what do you want me to speak ')
+        self.labell.pack()
+        self.entry = tk.Entry(self.root)
+        self.entry.pack()
+        self.button = tk.Button(self.root, text='Speak!',command=self.clicked)
+        self.button.pack()
+    def speak(self, text):
+        engine.say(text)
+        engine.runAndWait()
+    def clicked(self):
+        text = self.entry.get()
+        self.speak(text)
+if __name__ == '__main__':
+    wiget = Widget()
+    
